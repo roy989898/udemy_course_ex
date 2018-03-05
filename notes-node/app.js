6eb5dc6883e85e4c3a04e045c17d80dc13954bdd
roy989898 <roy989898@gmail.com>
2018-03-05 21:59:05 +0800

    require a 3 party

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,6 +1,7 @@
 console.log('Starting app');
 
 
+const _ = require('lodash');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./note');
@@ -9,4 +10,7 @@ let user = os.userInfo();
 
 let res = notes.addNote();
 console.log(res);
+
+let ans=_.add(1,5);
+console.log(ans);
 
